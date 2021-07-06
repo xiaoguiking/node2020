8db26621801b2ebf62fc1f37029d585da22e809d
lihuazhi <lihuazhi@xiaodutv.com>
2021-07-06 19:18:44 +0800

    feat: getChannels api

--- node-express/api/controller/public/book/getChannels.js
+++ node-express/api/controller/public/book/getChannels.js
@@ -1,1 +1,34 @@
-const common = require("../../common");
+/**
+ * @api {get} api/book/getChannels  获取文章频道/类型
+ * @apiGroup Group   Book
+ * @apiDescription 添加书籍信息
+ *
+ * @apiParam {String} typeid  频道/类型
+ */
+
+ const common = require("../../common");
+ 
+ const { BookModel } = require("../../../model/index")
+ 
+ module.exports.getChannels = function (req, res) {
+    const {typeid } =req.body
+    BookModel.find({typeid}, function (err, data) {
+        if (err) {
+            common.sendJsonResponse(res, 500, err);
+        }
+        if (data.length > 0) {
+           common.sendResponse(res, 200, {
+               error: "1",
+               message: "查询成功",
+               list: data
+           })
+        } else {
+           common.sendResponse(res, 200, {
+               error: "1",
+               message: "暂无数据",
+               list: data
+           }) 
+        }
+    })
+ }
+ 
--- node-express/api/routes/public/book/getChannels.js
+++ node-express/api/routes/public/book/getChannels.js
@@ -1,8 +1,8 @@
-// 获取文章频道
+// 获取文章频道 类型
 'use strict';
 var express = require('express');
-// var controller = require('../../../controller/public/book/getChannels');
+var controller = require('../../../controller/public/book/getChannels');
 var router = express.Router();
-// router.post('/getChannels', controller.getChannels);
+router.get('/getChannels', controller.getChannels);
 
 module.exports = router;
